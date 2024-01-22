f028aba85fa9496f817da54d3cb8e485fa3e3f2a
Gennifire <Jenniferdee86@gmail.com>
2024-01-22 11:54:14 +0000

    Add project files.

---
+++ BlogProject.API/Program.cs
@@ -0,0 +1,34 @@
+//We first add services to the container
+//Dotnet supports the dependency injection software design pattern,
+//which is a technique for achieving inversion of control between
+//classes and their dependencies.
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+//
+
+
+
+// Configure the HTTP request pipeline.
+// Pipeline to handle requests and responses.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
