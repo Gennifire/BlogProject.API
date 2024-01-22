71f956ada59d3e52041c6d5d585ac5bb16761862
Gennifire <Jenniferdee86@gmail.com>
2024-01-22 12:37:03 +0000

    DbContext

---
+++ BlogProject.API/Data/ApplicationDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using BlogProject.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.API.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Category> Catergories { get; set; }
+    }
+}
