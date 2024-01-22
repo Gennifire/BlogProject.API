86dd8b86978d03f87828d435aebe6c3f8508544c
Gennifire <Jenniferdee86@gmail.com>
2024-01-22 14:25:28 +0000

    Http Post

--- BlogProject.API/Controllers/CategoriesController.cs
+++ BlogProject.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlogProject.API.Data;
+using BlogProject.API.Models.Domain;
+using BlogProject.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.API.Controllers
@@ -7,6 +10,35 @@ namespace BlogProject.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ApplicationDbcontext _dbContext;
 
+        public CategoriesController(ApplicationDbcontext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            //map DTO to domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _dbContext.Catergories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+
+            //Domain model to DTO
+            var response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
